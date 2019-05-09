4656845fb878af4ecba7bb0f23eac527aa45b3c1
jll9qw <lloyd804@gmail.com>
2019-05-09 11:30:48 -0400

    connected to server

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 // Dependencies 
 const express = require("express");
 const app = express();
-const PORT = 3030;
+const PORT = 3550;
 
 // Allows data to be sent to server as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-
+app.listen(PORT, function() {
+    console.log('App listening on PORT ' + PORT);
+  });
 // $('#myModal').on('hidden.bs.modal', function (e) {
 //     if (!data) return e.preventDefault() // stops modal from being shown
 //   })
