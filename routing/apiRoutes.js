7109834eceb0272355172c5dffaae655dd4fef92
jll9qw <lloyd804@gmail.com>
2019-05-12 16:50:32 -0400

    added friends html

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,20 +1,20 @@
-// var tables = require('../data/tables');
+var friends = require('../public/friends');
 
 // module.exports = function(app) {
 // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 
 
 // app.get('/api/waitlist', function(req, res) {
-//     res.json(tables.waitingTables);
+//     res.json(friends.waitingfriends);
 //   });
-
-
-// app.post('/api/tables', function(req, res) {
-//     var newTable = tables.addTable(req.body);
-//     res.json(newTable);
-//   });
-
 
 // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+
+app.post('/api/friends', function(req, res) {
+    var newFriends = friends.addTable(req.body);
+    res.json(newFriends);
+  });
+
+
 
 // }
