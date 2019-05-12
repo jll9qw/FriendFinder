70029d5795babd716f6547822822d07c8bf3cf80
jll9qw <lloyd804@gmail.com>
2019-05-12 16:55:14 -0400

    added friends obj

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var friendsData = [
+    {
+      customerName: 'Ahmed',
+      customerEmail: 'ahmed@example.com',
+      customerID: 'afhaque89',
+      phoneNumber: '000-000-0000',
+      isWaiting: false
+    },
+    {
+      customerName: 'Saima',
+      customerEmail: 'saima@example.com',
+      phoneNumber: '000-000-0000',
+      customerID: 'saimaCool',
+      isWaiting: true
+    }
+  ];
+  
+  module.exports = friendsData;
