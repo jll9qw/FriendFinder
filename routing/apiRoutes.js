ad50893484d169fc647f71e17932feed5a8ad212
jll9qw <lloyd804@gmail.com>
2019-05-13 13:19:43 -0400

    post err WIP

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -10,11 +10,21 @@ app.get('/api/friends', function(req, res) {
 
 // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 
-// app.post('/api/friends', function(req, res) {
-// //     var newFriends = friends.addTable(req.body);
-//     res.json(newFriends);
-//   });
-
+app.post('/api/friends', function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newFriends = req.body;
+  
+    // Using a RegEx Pattern to remove spaces from newFriends
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newFriends.routeName = newFriends.name.replace(/\s+/g, '').toLowerCase();
+  
+    console.log(newFriends);
+  
+    friends.push(newFriends);
+  
+    res.json(newFriends);
+  });
 
 
 }
