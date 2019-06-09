24e86f04238d23c2beb5cd768f65ba683deb7a2e
jll9qw <lloyd804@gmail.com>
2019-06-09 10:13:16 -0400

    updated survey route

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.get("/survey", function(req, res) {
     // A GET Route to /survey which should display the survey page.
 
-    res.sendFile(path.join(__dirname, "../public/survey.html"));
+    res.sendFile(path.join(__dirname,"../public/survey.html"));
   });
 
   // A default, catch-all route that leads to home.html which displays the home page.
