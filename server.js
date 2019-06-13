00eaf3c1e13605c82c15c8322b60806bde4f2c60
jll9qw <lloyd804@gmail.com>
2019-06-13 01:52:18 -0400

    debugged server error

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-require(path.join(__dirname, '/routing/apiRoutes'))(app);
-require(path.join(__dirname, '/routing/htmlRoutes'))(app);
+require(path.join(__dirname, "./app/routing/apiRoutes"))(app);
+require(path.join(__dirname, "./app/routing/htmlRoutes"))(app);
 
 app.listen(PORT, function() {
     console.log('App listening on PORT ' + PORT);
