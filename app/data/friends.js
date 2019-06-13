167b49d48cc0ec2cdefacc456001f79d44de2dce
jll9qw <lloyd804@gmail.com>
2019-06-12 22:25:43 -0400

    re-structured app and app  dir

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,13 +1,47 @@
 var friendsData = [
   {
-   name: "Ahmed",
-    pic: "ahmed@example.com",
-    score: []
+    name: "sally",
+    pic: "www.msn.com",
+    score: [
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1"
+    ]
   },
   {
-    name: "Saima",
-    pic: "saima@example.com",
-    score: []
+    name: "billy",
+    pic: "www.msn.com",
+    score: [
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1"
+    ]
+  },
+  {
+    name: "Ahmed",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+  },
+  {
+    name: "Jacob Deming",
+    photo:
+      "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
   }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,4 @@
-var friends = require('../app/data/friends');
+var friends = require('../data/friends');
 
 module.exports = function(app) {
 // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
@@ -8,22 +8,29 @@ app.get('/api/friends', function(req, res) {
 
 
 
+
 // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 app.post('/api/friends', function(req, res) {
     
-    newfriendData = req.body;
+   var newfriendData = req.body;
 
   
   var match ={
     index: 0,
     least_difference: 0
   };
+
+  for (let i = 0; i < friends.length; i++) {
+    const element = friends[i];
     
-  friends.forEach((friend, i) => {
+
     var difference = 0;
-    friend.score.forEach((score, j) => {
-      difference += Math.abs(newfriendData.score[j] - score);
-    });
+
+    for (let j = 0; j < element.scores.length; j++) {
+      const element = scores[j];
+      
+      difference += Math.abs(newfriendData.scores[j] - parseInt(score));
+    };
     if (i === 0) {
       match.index = i;
       match.least_difference = difference;
@@ -31,15 +38,15 @@ app.post('/api/friends', function(req, res) {
       match.index = i;
       match.least_difference = difference;
     } else return;
-  });
+
 
   best_match = friends[match.index];
   friends.push(newfriendData);
   res.json(best_match)
 
-  
-
-  });
 
 
+  };
+
+})
 }
